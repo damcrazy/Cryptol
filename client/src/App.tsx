3bd0e080b731e22efbab37086a3106d1bee9f14c
Blazeleo <satviksams@gmail.com>
2022-08-07 20:54:48 +0530

    Navbar Pre-merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,13 @@ import { useState } from 'react'
 import './App.css'
 
 //components
-import Navbar from './Components/Navbar/Navbar'
+import MainNavbar from './Components/Navbar/Navbar'
 
 function App() {
-
-
   return (
     <div className="App">
       <div id="particles-js"></div>
-      <Navbar /> 
+      <MainNavbar /> 
     </div>
   )
 }
--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -1,22 +1,79 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import "./Navbar.css";
+function MainNavbar() {
+  let user = 0;
+const loginStyle = {
+  marginTop: "10px",
+  border: "0",
+  backgroundColor: "#791cf8",
+  paddingLeft: "20px",
+  paddingRight: "20px",
+  paddingTop: "5px",
+  paddingBottom: "5px",
+}
 
-//css
-import './Navbar.css'
 
-export default function Navbar() {
   return (
     <>
-        <div className="navbar">
-            <div className="navbar-logo">
-                <img src="" alt="logo" />
-            </div>
-            <div className="navbar-links">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-            </div>
-        </div>
-
+      <Navbar bg="" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="Logo"
+              src="/images/Logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Cryptol
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Exchange</Nav.Link>
+              <Nav.Link href="#link">Learn</Nav.Link>
+            </Nav>
+            <Nav>
+              {user ? (
+                <NavDropdown
+                  title={
+                    <span style={{ padding: "5px" }}>
+                      <img
+                        alt="Logo"
+                        src="/images/person.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                      />
+                      <span style={{ padding: "10px" }}>Username</span>
+                    </span>
+                  }
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item href="#action/3.1">
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Wallets
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Read T&C
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <button className="outline">Login</button>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  )
+  );
 }
+
+export default MainNavbar;
