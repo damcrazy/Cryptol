d19949616c34f62de34fe42d81d1e38853be4e71
Kalyan <kalyanb2000@gmail.com>
2022-08-07 20:30:26 +0530

    something_big1.0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,16 @@ import './App.css'
 
 //components
 import Navbar from './Components/Navbar/Navbar'
+import Logo_cool from './Components/Logo-cool/Logo_cool'
 
 function App() {
 
 
   return (
     <div className="App">
-      <div id="particles-js"></div>
       <Navbar /> 
+      <div id="particles-js"></div>
+      <Logo_cool/>
     </div>
   )
 }
